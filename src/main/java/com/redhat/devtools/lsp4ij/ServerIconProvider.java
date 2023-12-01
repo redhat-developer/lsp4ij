3749a34c24781f66a500ac4a96bedead1ff58b23
Angelo <angelo.zerr@gmail.com>
2023-12-01 09:18:29 +0100

    refactor: Remove 'intellij' from package name. (#3)
    Signed-off-by: azerr <azerr@redhat.com>
    Co-authored-by: azerr <azerr@redhat.com>

---
+++ src/main/java/com/redhat/devtools/lsp4ij/ServerIconProvider.java
@@ -0,0 +1,8 @@
+package com.redhat.devtools.lsp4ij;
+
+import javax.swing.*;
+
+public interface ServerIconProvider {
+
+    Icon getIcon();
+}
---
+++ src/test/java/com/redhat/devtools/lsp4ij/operations/completion/snippet/PlaceholderTest.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Red Hat Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package com.redhat.devtools.lsp4ij.operations.completion.snippet;
+
+import com.redhat.devtools.lsp4ij.operations.completion.snippet.handler.LspSnippetNode;
+import org.junit.Test;
+
+import static com.redhat.devtools.lsp4ij.operations.completion.snippet.LspSnippetAssert.*;
+
+public class PlaceholderTest {
+
+    @Test
+    public void simpleText() {
+        LspSnippetNode[] actual = LspSnippetAssert.parse("${1:name}");
+        assertEquals(actual, LspSnippetAssert.placeholder(1, "name", 1));
+    }
+
+}
