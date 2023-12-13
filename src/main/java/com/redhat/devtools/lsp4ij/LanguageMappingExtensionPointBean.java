f1ce8198c223a2eef02d994130cd7475a36fcc6d
azerr <azerr@redhat.com>
2023-12-13 11:43:42 +0100

    feat: server bundle
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/lsp4ij/LanguageMappingExtensionPointBean.java
+++ src/main/java/com/redhat/devtools/lsp4ij/LanguageMappingExtensionPointBean.java
@@ -1,26 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
 package com.redhat.devtools.lsp4ij;
 
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.openapi.extensions.RequiredElement;
 import com.intellij.serviceContainer.BaseKeyedLazyInstance;
 import com.intellij.util.xmlb.annotations.Attribute;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Language mapping extension point.
+ */
 public class LanguageMappingExtensionPointBean extends BaseKeyedLazyInstance<DocumentMatcher> {
 
     private static final DocumentMatcher DEFAULT_DOCUMENT_MATCHER = (file,project) -> true;
 
     public static final ExtensionPointName<LanguageMappingExtensionPointBean> EP_NAME = ExtensionPointName.create("com.redhat.devtools.lsp4ij.languageMapping");
 
-    @Attribute("id")
-    public String id;
-
+    /**
+     * The language mapped with the language server {{@link #serverId}server id}.
+     */
     @Attribute("language")
-    public String language;
+    @RequiredElement
+    public String languageId;
 
+    /**
+     * The language server mapped with the language {{@link #languageId}language id}.
+     */
     @Attribute("serverId")
+    @RequiredElement
     public String serverId;
 
+    /**
+     * The {@link DocumentMatcher document matcher}.
+     */
     @Attribute("documentMatcher")
     public String documentMatcher;
 
