bd97606e997370762620a37a56be78d5a1480d77
Fred Bricon <fbricon@gmail.com>
2024-01-09 16:42:10 +0100

    feat: provide color icons for completion
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/java/com/redhat/devtools/lsp4ij/ui/IconMapperTest.java
+++ src/test/java/com/redhat/devtools/lsp4ij/ui/IconMapperTest.java
@@ -13,20 +13,73 @@
  *******************************************************************************/
 package com.redhat.devtools.lsp4ij.ui;
 
+import com.intellij.icons.AllIcons;
+import com.intellij.util.ui.ColorIcon;
+import org.eclipse.lsp4j.CompletionItem;
 import org.eclipse.lsp4j.CompletionItemKind;
+import org.eclipse.lsp4j.MarkupContent;
 import org.junit.Test;
 
+import javax.swing.*;
+
 import static com.redhat.devtools.lsp4ij.ui.IconMapper.getIcon;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class IconMapperTest {
 
     @Test
     public void getIconTest()  {
-        assertNull(getIcon(null));
+        assertNull(getIcon((CompletionItemKind)null));
         for (CompletionItemKind value : CompletionItemKind.values()) {
             assertNotNull(getIcon(value), "Missing matching icon for "+value);
         }
+    }
+
+    @Test
+    public void getColorIconTest()  {
+        String color = "#FFFFFF";
+
+        CompletionItem item = new CompletionItem();
+        item.setKind(CompletionItemKind.Color);
+
+        //Test string documentation
+        item.setDocumentation(color);
+        Icon whiteIcon = getIcon(item);
+        assertNotNull(whiteIcon, "Should have an icon");
+        assertNotEquals(AllIcons.Nodes.EmptyNode, whiteIcon);
+        assertTrue(whiteIcon instanceof ColorIcon, "Unexpected icon instance: "+ whiteIcon.getClass());
+
+        //Lower case color
+        item.setDocumentation(color.toLowerCase());
+        Icon sameWhiteIcon = getIcon(item);
+        assertEquals(whiteIcon, sameWhiteIcon);
+
+        //Other color
+        item.setDocumentation("#000000");
+        Icon blackIcon = getIcon(item);
+        assertNotEquals(whiteIcon, blackIcon);
+
+        //MarkupContent
+        item = new CompletionItem();
+        item.setKind(CompletionItemKind.Color);
+        item.setDocumentation(new MarkupContent("markdown", color ));
+        Icon markdownIcon = getIcon(item);
+        assertEquals(whiteIcon, markdownIcon);
+
+
+        //Label
+        item = new CompletionItem();
+        item.setKind(CompletionItemKind.Color);
+        item.setLabel(color);
+        Icon labelIcon = getIcon(item);
+        assertEquals(whiteIcon, labelIcon);
+
+        //Invalid color
+        item = new CompletionItem();
+        item.setKind(CompletionItemKind.Color);
+        item.setLabel("#Nooope");
+        Icon badColorIcon = getIcon(item);
+        assertEquals(AllIcons.Nodes.EmptyNode, badColorIcon);
     }
 }
