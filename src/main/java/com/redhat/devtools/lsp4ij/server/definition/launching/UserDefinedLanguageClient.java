c7863c4985a3d65ebfb2980c6ab0b126251c6826
Fred Bricon <fbricon@gmail.com>
2024-03-12 12:12:57 +0100

    feat: add rust-analyzer template in dev mode
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/main/java/com/redhat/devtools/lsp4ij/server/definition/launching/UserDefinedLanguageClient.java
+++ src/main/java/com/redhat/devtools/lsp4ij/server/definition/launching/UserDefinedLanguageClient.java
@@ -14,7 +14,12 @@ import com.intellij.openapi.project.Project;
 import com.redhat.devtools.lsp4ij.LanguageServersRegistry;
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl;
 import com.redhat.devtools.lsp4ij.lifecycle.LanguageServerLifecycleManager;
+import org.eclipse.lsp4j.ConfigurationParams;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * User defined language client implementation used to track configuration changes
@@ -36,5 +41,11 @@ public class UserDefinedLanguageClient extends LanguageClientImpl {
         super.dispose();
         LanguageServerLifecycleManager.getInstance(getProject()).removeLanguageServerLifecycleListener(languageServerStartedListener);
         LanguageServersRegistry.getInstance().removeLanguageServerDefinitionListener(languageServerStartedListener);
+    }
+
+    //Hack so that LS requesting workspace/configuration without checking client capability don't cause an error (looking at you rust-analyzer)
+    @Override
+    public CompletableFuture<List<Object>> configuration(ConfigurationParams configurationParams) {
+        return CompletableFuture.completedFuture(Collections.emptyList());
     }
 }
