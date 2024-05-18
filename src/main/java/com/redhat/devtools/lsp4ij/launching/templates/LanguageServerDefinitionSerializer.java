12b37baa10902d3495ebfd3ee4addecb0804f951
Mitja Leino <mituuuuz@hotmail.com>
2024-05-18 12:38:34 +0300

    Add license

--- src/main/java/com/redhat/devtools/lsp4ij/launching/templates/LanguageServerDefinitionSerializer.java
+++ src/main/java/com/redhat/devtools/lsp4ij/launching/templates/LanguageServerDefinitionSerializer.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat Inc. and others.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Mitja Leino <mitja.leino@hotmail.com> - Initial API and implementation
+ *******************************************************************************/
 package com.redhat.devtools.lsp4ij.launching.templates;
 
 import com.google.gson.*;
