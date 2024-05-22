94602da83537818112b8624d9478ec93e2b49906
Fred Bricon <fbricon@gmail.com>
2024-05-22 11:07:43 +0200

    chore: reduce mock server sleep time to speed tests up
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/java/com/redhat/devtools/lsp4ij/fixtures/LSPRenameFixtureTestCase.java
+++ src/test/java/com/redhat/devtools/lsp4ij/fixtures/LSPRenameFixtureTestCase.java
@@ -141,7 +141,7 @@ public abstract class LSPRenameFixtureTestCase extends LSPCodeInsightFixtureTest
                               boolean waitFor) {
         updateRenameCapabilities(jsonPrepareRenameResponse);
 
-        MockLanguageServer.INSTANCE.setTimeToProceedQueries(1000);
+        MockLanguageServer.INSTANCE.setTimeToProceedQueries(450);
         PsiFile file = myFixture.configureByText(fileName, text);
 
         // Prepare rename response
