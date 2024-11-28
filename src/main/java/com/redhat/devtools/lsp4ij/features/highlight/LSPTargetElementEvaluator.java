e32d6bffcab7f7ab0ef6ed811aa7dde3dbfd0f46
Scott Wells <scott@illuminatedcloud.com>
2024-11-28 09:58:41 -0600

    Issue 636 - Implemented PR review suggestions.

--- src/main/java/com/redhat/devtools/lsp4ij/features/highlight/LSPTargetElementEvaluator.java
+++ src/main/java/com/redhat/devtools/lsp4ij/features/highlight/LSPTargetElementEvaluator.java
@@ -1,19 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package com.redhat.devtools.lsp4ij.features.highlight;
 
 import com.intellij.codeInsight.TargetElementEvaluatorEx2;
-import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.util.containers.ContainerUtil;
+import com.redhat.devtools.lsp4ij.LSPIJUtils;
 import com.redhat.devtools.lsp4ij.LanguageServersRegistry;
 import com.redhat.devtools.lsp4ij.client.ProjectIndexingManager;
+import com.redhat.devtools.lsp4ij.features.LSPPsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.List;
-
+/**
+ * Finds the name identifier at the specified coordinates as the target PSI element.
+ */
 public class LSPTargetElementEvaluator extends TargetElementEvaluatorEx2 {
     @Override
     public @Nullable PsiElement adjustReferenceOrReferencedElement(@NotNull PsiFile file, @NotNull Editor editor, int offset, int flags, @Nullable PsiElement refElement) {
@@ -25,9 +36,8 @@ public class LSPTargetElementEvaluator extends TargetElementEvaluatorEx2 {
             return null;
         }
 
-        // Try to find the highlighted identifier at the caret and return that fake PSI element
-        HighlightUsagesHandlerBase<LSPHighlightPsiElement> highlightUsagesHandler = new LSPHighlightUsagesHandlerFactory().createHighlightUsagesHandler(editor, file);
-        List<LSPHighlightPsiElement> targets = highlightUsagesHandler != null ? highlightUsagesHandler.getTargets() : Collections.emptyList();
-        return ContainerUtil.find(targets, target -> (target.getTextRange() != null) && target.getTextRange().contains(offset));
+        // Try to find the token at the caret and return fake PSI element for it
+        TextRange tokenTextRange = LSPIJUtils.getTokenTextRange(file, editor, offset);
+        return tokenTextRange != null ? new LSPPsiElement(file, tokenTextRange) : null;
     }
 }
