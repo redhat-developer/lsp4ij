6f443b7e85f5362c535bbe6da5a95b559f5755a3
Scott Wells <scott@illuminatedcloud.com>
2024-11-29 09:30:18 -0600

    Updated the date to 2024. There's still a handful of files with 2019 as their date. Is that correct? Should a copyright profile be added to the project so this is all maintained automatically, or minimally a copyright header be added to the Java file templates so that newly-added classes have it?

--- src/main/java/com/redhat/devtools/lsp4ij/features/workspaceSymbol/LSPWorkspaceImplementationsSearch.java
+++ src/main/java/com/redhat/devtools/lsp4ij/features/workspaceSymbol/LSPWorkspaceImplementationsSearch.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat, Inc.
+ * Copyright (c) 2024 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
