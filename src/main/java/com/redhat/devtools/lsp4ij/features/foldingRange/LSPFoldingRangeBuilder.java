3b0231ef9e07e3422ecec3699b1727158616d5cc
Scott Wells <scott@illuminatedcloud.com>
2024-12-05 15:08:50 -0600

    LSP code folding improvements for braced blocks and code block provider implementation based on that same information.

--- src/main/java/com/redhat/devtools/lsp4ij/features/foldingRange/LSPFoldingRangeBuilder.java
+++ src/main/java/com/redhat/devtools/lsp4ij/features/foldingRange/LSPFoldingRangeBuilder.java
@@ -18,6 +18,7 @@ import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.util.containers.ContainerUtil;
 import com.redhat.devtools.lsp4ij.LSPFileSupport;
 import com.redhat.devtools.lsp4ij.LSPIJUtils;
 import com.redhat.devtools.lsp4ij.client.ExecuteLSPFeatureStatus;
@@ -26,10 +27,13 @@ import org.eclipse.lsp4j.FoldingRange;
 import org.eclipse.lsp4j.FoldingRangeRequestParams;
 import org.eclipse.lsp4j.Position;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -44,6 +48,14 @@ public class LSPFoldingRangeBuilder extends CustomFoldingBuilder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LSPFoldingRangeBuilder.class);
 
+    // TODO: Can this be derived from the language server? If not, these are the standard for modern languages
+    private static final Map<Character, Character> BRACE_PAIR_CHARS_FWD = Map.of(
+            '{', '}',
+            '[', ']',
+            '(', ')'
+    );
+    private static final Map<Character, Character> BRACE_PAIR_CHARS_BWD = ContainerUtil.reverseMap(BRACE_PAIR_CHARS_FWD);
+
     @Override
     protected void buildLanguageFoldRegions(@NotNull List<FoldingDescriptor> descriptors,
                                             @NotNull PsiElement root,
@@ -54,8 +66,30 @@ public class LSPFoldingRangeBuilder extends CustomFoldingBuilder {
         }
 
         PsiFile file = root.getContainingFile();
+        List<FoldingRange> foldingRanges = getFoldingRanges(file);
+        if (!ContainerUtil.isEmpty(foldingRanges)) {
+            for (FoldingRange foldingRange : foldingRanges) {
+                TextRange textRange = getTextRange(foldingRange, document);
+                if (textRange != null) {
+                    String collapsedText = foldingRange.getCollapsedText();
+                    if (collapsedText != null) {
+                        descriptors.add(new FoldingDescriptor(root.getNode(), textRange, null, collapsedText));
+                    } else {
+                        descriptors.add(new FoldingDescriptor(root.getNode(), textRange));
+                    }
+                }
+            }
+        }
+    }
+
+    @NotNull
+    static List<FoldingRange> getFoldingRanges(@Nullable PsiFile file) {
         if (ProjectIndexingManager.canExecuteLSPFeature(file) != ExecuteLSPFeatureStatus.NOW) {
-            return;
+            return Collections.emptyList();
+        }
+
+        if (file == null) {
+            return Collections.emptyList();
         }
 
         // Consume LSP 'textDocument/foldingRanges' request
@@ -64,41 +98,98 @@ public class LSPFoldingRangeBuilder extends CustomFoldingBuilder {
         CompletableFuture<List<FoldingRange>> foldingRangesFuture = foldingRangeSupport.getFoldingRanges(params);
         try {
             waitUntilDone(foldingRangesFuture, file);
-        } catch (ProcessCanceledException e) {//Since 2024.2 ProcessCanceledException extends CancellationException so we can't use multicatch to keep backward compatibility
+        } catch (
+                ProcessCanceledException e) {//Since 2024.2 ProcessCanceledException extends CancellationException so we can't use multicatch to keep backward compatibility
             //TODO delete block when minimum required version is 2024.2
             foldingRangeSupport.cancel();
-            return;
+            return Collections.emptyList();
         } catch (CancellationException e) {
             // cancel the LSP requests textDocument/foldingRanges
             foldingRangeSupport.cancel();
-            return;
+            return Collections.emptyList();
         } catch (ExecutionException e) {
             LOGGER.error("Error while consuming LSP 'textDocument/foldingRanges' request", e);
-            return;
+            return Collections.emptyList();
         }
 
-        if (isDoneNormally(foldingRangesFuture)) {
-            // textDocument/foldingRanges has been collected correctly, create list of IJ FoldingDescriptor from LSP FoldingRange list
-            List<FoldingRange> foldingRanges = foldingRangesFuture.getNow(null);
-            if (foldingRanges != null) {
-                for (FoldingRange foldingRange : foldingRanges) {
-                    int start = getStartOffset(foldingRange, document);
-                    int end = getEndOffset(foldingRange, document);
-                    if (start >= end) {
-                        // same line, ignore the folding
-                        continue;
-                    }
-                    String collapsedText = foldingRange.getCollapsedText();
-                    if (collapsedText != null) {
-                        descriptors.add(new FoldingDescriptor(root.getNode(), new TextRange(start, end), null, collapsedText));
-                    } else {
-                        descriptors.add(new FoldingDescriptor(root.getNode(), new TextRange(start, end)));
-                    }
+        if (!isDoneNormally(foldingRangesFuture)) {
+            return Collections.emptyList();
+        }
+
+        // textDocument/foldingRanges has been collected correctly, create list of IJ FoldingDescriptor from LSP FoldingRange list
+        return foldingRangesFuture.getNow(null);
+    }
+
+    @Nullable
+    private static TextRange getTextRange(@NotNull FoldingRange foldingRange, @NotNull Document document) {
+        return getTextRange(foldingRange, document, null, null);
+    }
+
+    /**
+     * Returns the IDE text range for the LSP folding range, optionally bounded by the expected paired open/close braces.
+     *
+     * @param foldingRange   the LSP folding range
+     * @param document       the document
+     * @param openBraceChar  the optional open brace character
+     * @param closeBraceChar the optional paired close brace character
+     * @return the corresponding IDE text range, or null if no valid text range could be derived
+     */
+    @Nullable
+    static TextRange getTextRange(
+            @NotNull FoldingRange foldingRange,
+            @NotNull Document document,
+            @Nullable Character openBraceChar,
+            @Nullable Character closeBraceChar
+    ) {
+        TextRange textRange = null;
+
+        String documentText = document.getText();
+        int documentLength = documentText.length();
+
+        int start = LSPFoldingRangeBuilder.getStartOffset(foldingRange, document);
+        Character startChar = start > 0 ? documentText.charAt(start - 1) : null;
+        if ((startChar != null) && ((openBraceChar == null) || (startChar == openBraceChar))) {
+            // If necessary, infer the braces for this block
+            if ((openBraceChar == null) && isOpenBraceChar(startChar)) {
+                openBraceChar = startChar;
+                closeBraceChar = getCloseBraceChar(openBraceChar);
+            }
+
+            int end = LSPFoldingRangeBuilder.getEndOffset(foldingRange, document);
+            // The end offsets can fall a bit short, so look for the closing brace character
+            if (closeBraceChar != null) {
+                while ((end < documentLength) && (documentText.charAt(end) != closeBraceChar)) {
+                    end++;
+                }
+            }
+            if (end >= start) {
+                Character endChar = end < documentLength ? documentText.charAt(end) : null;
+                if ((endChar != null) && ((closeBraceChar == null) || (endChar == closeBraceChar))) {
+                    textRange = TextRange.create(start, end);
                 }
             }
         }
+
+        return textRange;
     }
 
+    static boolean isOpenBraceChar(@Nullable Character character) {
+        return (character != null) && BRACE_PAIR_CHARS_FWD.keySet().contains(character);
+    }
+
+    static boolean isCloseBraceChar(@Nullable Character character) {
+        return (character != null) && BRACE_PAIR_CHARS_BWD.keySet().contains(character);
+    }
+
+    @Nullable
+    static Character getOpenBraceChar(@Nullable Character closeBraceChar) {
+        return closeBraceChar != null ? BRACE_PAIR_CHARS_BWD.get(closeBraceChar) : null;
+    }
+
+    @Nullable
+    static Character getCloseBraceChar(@Nullable Character openBraceChar) {
+        return openBraceChar != null ? BRACE_PAIR_CHARS_FWD.get(openBraceChar) : null;
+    }
 
     private static int getStartOffset(@NotNull FoldingRange foldingRange, @NotNull Document document) {
         if (foldingRange.getStartCharacter() == null) {
@@ -128,5 +219,4 @@ public class LSPFoldingRangeBuilder extends CustomFoldingBuilder {
     public boolean isDumbAware() {
         return false;
     }
-
 }
