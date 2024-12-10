74baeeeab6974ab11252f31d64c0dcdad24c1715
Scott Wells <scott@illuminatedcloud.com>
2024-12-10 10:48:51 -0600

    Issue 673 - Added unit tests for LSP-based code folding.

---
+++ src/test/java/com/redhat/devtools/lsp4ij/features/foldingRange/TypeScriptFoldingRangeTest.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package com.redhat.devtools.lsp4ij.features.foldingRange;
+
+import com.intellij.openapi.util.TextRange;
+import com.redhat.devtools.lsp4ij.fixtures.LSPFoldingRangeFixtureTestCase;
+
+/**
+ * Selection range tests by emulating LSP 'textDocument/foldingRange' responses from the typescript-language-server.
+ */
+public class TypeScriptFoldingRangeTest extends LSPFoldingRangeFixtureTestCase {
+
+    private static final String DEMO_TS_FILE_NAME = "demo.ts";
+    // language=typescript
+    private static final String DEMO_TS_FILE_BODY = """
+            export class Demo {
+                demo() {
+                    console.log('demo');
+                }
+            }
+            """;
+
+    // Demo class braced block exclusive of braces
+    private static final TextRange DEMO_CLASS_BODY_TEXT_RANGE = TextRange.create(19, 68);
+    // demo() function braced block exclusive of braces
+    private static final TextRange DEMO_METHOD_BODY_TEXT_RANGE = TextRange.create(32, 66);
+
+    public TypeScriptFoldingRangeTest() {
+        super("*.ts");
+    }
+
+    public void testFoldingRanges() {
+        assertFoldingRanges(
+                DEMO_TS_FILE_NAME,
+                DEMO_TS_FILE_BODY,
+                // language=json
+                """
+                        
+                        [
+                          {
+                            "startLine": 0,
+                            "endLine": 3
+                          },
+                          {
+                            "startLine": 1,
+                            "endLine": 2
+                          }
+                        ]
+                        """,
+                DEMO_CLASS_BODY_TEXT_RANGE,
+                DEMO_METHOD_BODY_TEXT_RANGE
+        );
+    }
+
+    public void testFoldingRanges_collapsedText() {
+        assertFoldingRanges(
+                DEMO_TS_FILE_NAME,
+                DEMO_TS_FILE_BODY,
+                // language=json
+                """
+                        
+                        [
+                          {
+                            "startLine": 0,
+                            "endLine": 3,
+                            "collapsedText": "classBody"
+                          },
+                          {
+                            "startLine": 1,
+                            "endLine": 2,
+                            "collapsedText": "methodBody"
+                          }
+                        ]
+                        """,
+                DEMO_CLASS_BODY_TEXT_RANGE,
+                DEMO_METHOD_BODY_TEXT_RANGE
+        );
+    }
+}
---
+++ src/test/java/com/redhat/devtools/lsp4ij/fixtures/LSPFoldingRangeFixtureTestCase.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.lsp4ij.fixtures;
+
+import com.google.gson.reflect.TypeToken;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.FoldRegion;
+import com.intellij.openapi.editor.FoldingModel;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.EditorTestUtil;
+import com.redhat.devtools.lsp4ij.JSONUtils;
+import com.redhat.devtools.lsp4ij.LanguageServiceAccessor;
+import com.redhat.devtools.lsp4ij.mock.MockLanguageServer;
+import org.eclipse.lsp4j.FoldingRange;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Base class test case to test LSP 'textDocument/foldingRange' feature.
+ */
+public abstract class LSPFoldingRangeFixtureTestCase extends LSPCodeInsightFixtureTestCase {
+
+    public LSPFoldingRangeFixtureTestCase(String... fileNamePatterns) {
+        super(fileNamePatterns);
+    }
+
+    protected void assertFoldingRanges(@NotNull String fileName,
+                                       @NotNull String fileBody,
+                                       @NotNull String mockFoldingRangesJson,
+                                       @NotNull TextRange... expectedFoldingTextRanges) {
+        MockLanguageServer.INSTANCE.setTimeToProceedQueries(100);
+        List<FoldingRange> mockFoldingRanges = JSONUtils.getLsp4jGson().fromJson(mockFoldingRangesJson, new TypeToken<List<FoldingRange>>() {
+        }.getType());
+        MockLanguageServer.INSTANCE.setFoldingRanges(mockFoldingRanges);
+
+        PsiFile file = myFixture.configureByText(fileName, fileBody);
+        Editor editor = myFixture.getEditor();
+
+        // Initialize the language server
+        try {
+            LanguageServiceAccessor.getInstance(file.getProject())
+                    .getLanguageServers(file.getVirtualFile(), null, null)
+                    .get(5000, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        EditorTestUtil.buildInitialFoldingsInBackground(editor);
+        FoldingModel foldingModel = editor.getFoldingModel();
+        FoldRegion[] foldRegions = foldingModel.getAllFoldRegions();
+
+        assertEquals(expectedFoldingTextRanges.length, foldRegions.length);
+
+        for (int i = 0; i < foldRegions.length; i++) {
+            FoldRegion actualFoldRegion = foldRegions[i];
+
+            // Check the text range
+            TextRange expectedFoldingTextRange = expectedFoldingTextRanges[i];
+            assertEquals(expectedFoldingTextRange, actualFoldRegion.getTextRange());
+
+            // Check the placeholder text
+            FoldingRange expectedFoldingRange = mockFoldingRanges.get(i);
+            String expectedPlaceholderText = StringUtil.isNotEmpty(expectedFoldingRange.getCollapsedText()) ? expectedFoldingRange.getCollapsedText() : "...";
+            assertEquals(expectedPlaceholderText, actualFoldRegion.getPlaceholderText());
+        }
+    }
+}
