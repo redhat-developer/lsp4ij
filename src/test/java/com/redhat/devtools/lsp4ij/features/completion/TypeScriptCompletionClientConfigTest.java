7d7da4154361360d266cb73cf2dbf9ffc048afb2
Scott Wells <scott@illuminatedcloud.com>
2025-01-09 15:39:38 -0600

    Added tests for multi-arg invocations.

--- src/test/java/com/redhat/devtools/lsp4ij/features/completion/TypeScriptCompletionClientConfigTest.java
+++ src/test/java/com/redhat/devtools/lsp4ij/features/completion/TypeScriptCompletionClientConfigTest.java
@@ -8,6 +8,7 @@
  * Contributors:
  * Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
+
 package com.redhat.devtools.lsp4ij.features.completion;
 
 import com.redhat.devtools.lsp4ij.fixtures.LSPCompletionClientConfigFixtureTestCase;
@@ -19,8 +20,13 @@ public class TypeScriptCompletionClientConfigTest extends LSPCompletionClientCon
 
     private static final String TEST_FILE_NAME = "test.ts";
 
-    private static final String ABS_FILE_BODY_BEFORE = "Math.<caret>";
+    public TypeScriptCompletionClientConfigTest() {
+        super("*.ts");
+    }
 
+    // SINGLE ARGUMENT TESTS
+
+    private static final String ABS_FILE_BODY_BEFORE = "Math.<caret>";
     // language=json
     private static final String ABS_MOCK_TEXT_COMPLETION_JSON = """
             {
@@ -38,7 +44,6 @@ public class TypeScriptCompletionClientConfigTest extends LSPCompletionClientCon
               ]
             }
             """;
-
     // language=json
     private static final String ABS_MOCK_COMPLETION_ITEM_RESOLVE_JSON = """
             {
@@ -63,11 +68,7 @@ public class TypeScriptCompletionClientConfigTest extends LSPCompletionClientCon
             }
             """;
 
-    public TypeScriptCompletionClientConfigTest() {
-        super("*.ts");
-    }
-
-    public void testUseTemplateForSingleArgumentDefault() {
+    public void testUseTemplateForSingleArgumentDefault_singleArg() {
         assertTemplateForArguments(
                 TEST_FILE_NAME,
                 ABS_FILE_BODY_BEFORE,
@@ -78,13 +79,108 @@ public class TypeScriptCompletionClientConfigTest extends LSPCompletionClientCon
         );
     }
 
-    public void testUseTemplateForSingleArgumentDisabled() {
+    public void testUseTemplateForSingleArgumentDisabled_singleArg() {
         assertTemplateForArguments(
                 TEST_FILE_NAME,
                 ABS_FILE_BODY_BEFORE,
                 "Math.abs(<caret>)",
                 ABS_MOCK_TEXT_COMPLETION_JSON,
                 ABS_MOCK_COMPLETION_ITEM_RESOLVE_JSON,
+                clientConfig -> clientConfig.completion.useTemplateForSingleArgument = false
+        );
+    }
+
+    // MULTIPLE ARGUMENTS TESTS
+
+    private static final String POW_FILE_BODY_BEFORE = "Math.<caret>";
+    // language=json
+    private static final String POW_MOCK_TEXT_COMPLETION_JSON = """
+            {
+              "isIncomplete": false,
+              "items": [
+                {
+                  "label": "pow",
+                  "kind": 2,
+                  "sortText": "11",
+                  "filterText": ".pow",
+                  "insertTextFormat": 2,
+                  "textEdit": {
+                    "range": {
+                      "start": {
+                        "line": 0,
+                        "character": 4
+                      },
+                      "end": {
+                        "line": 0,
+                        "character": 5
+                      }
+                    },
+                    "newText": ".pow"
+                  },
+                  "data": {
+                    "cacheId": 32
+                  }
+                }
+              ]
+            }
+            """;
+    // language=json
+    private static final String POW_MOCK_COMPLETION_ITEM_RESOLVE_JSON = """
+            {
+              "label": "pow",
+              "kind": 2,
+              "detail": "(method) Math.pow(x: number, y: number): number",
+              "documentation": {
+                "kind": "markdown",
+                "value": "Returns the value of a base expression taken to a specified power.\\n\\n*@param* `x` — The base value of the expression.  \\n\\n*@param* `y` — The exponent value of the expression."
+              },
+              "sortText": "11",
+              "filterText": ".pow",
+              "insertText": ".pow(${1:x}, ${2:y})$0",
+              "insertTextFormat": 2,
+              "textEdit": {
+                "range": {
+                  "start": {
+                    "line": 0,
+                    "character": 4
+                  },
+                  "end": {
+                    "line": 0,
+                    "character": 5
+                  }
+                },
+                "newText": ".pow(${1:x}, ${2:y})$0"
+              },
+              "data": {
+                "file": "test.ts",
+                "line": 1,
+                "offset": 6,
+                "entryNames": [
+                  "pow"
+                ]
+              }
+            }
+            """;
+
+    public void testUseTemplateForSingleArgumentDefault_multipleArgs() {
+        assertTemplateForArguments(
+                TEST_FILE_NAME,
+                POW_FILE_BODY_BEFORE,
+                "Math.pow(x, y)<caret>",
+                POW_MOCK_TEXT_COMPLETION_JSON,
+                POW_MOCK_COMPLETION_ITEM_RESOLVE_JSON,
+                null // Use default configuration which has single arg templates enabled
+        );
+    }
+
+    public void testUseTemplateForSingleArgumentDisabled_multipleArgs() {
+        assertTemplateForArguments(
+                TEST_FILE_NAME,
+                POW_FILE_BODY_BEFORE,
+                // Because this has multiple arguments, the template will still be used
+                "Math.pow(x, y)<caret>",
+                POW_MOCK_TEXT_COMPLETION_JSON,
+                POW_MOCK_COMPLETION_ITEM_RESOLVE_JSON,
                 clientConfig -> clientConfig.completion.useTemplateForSingleArgument = false
         );
     }
