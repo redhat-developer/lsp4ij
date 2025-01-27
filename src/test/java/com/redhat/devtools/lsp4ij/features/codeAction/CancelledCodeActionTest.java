9cfa6c6db187b88e340ede40c312beb86883c2f5
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2025-01-27 18:10:15 +0530

    Added copyright section with updated year

--- src/test/java/com/redhat/devtools/lsp4ij/features/codeAction/CancelledCodeActionTest.java
+++ src/test/java/com/redhat/devtools/lsp4ij/features/codeAction/CancelledCodeActionTest.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ ******************************************************************************/
 package com.redhat.devtools.lsp4ij.features.codeAction;
 
 import com.redhat.devtools.lsp4ij.fixtures.LSPCodeInsightFixtureTestCase;
