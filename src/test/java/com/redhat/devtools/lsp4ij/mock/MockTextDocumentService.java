faab95e9bac8813151ace222e639cc1af306e52c
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2025-01-28 15:53:52 +0530

    Updated copyright year in MockTextDocumentService

--- src/test/java/com/redhat/devtools/lsp4ij/mock/MockTextDocumentService.java
+++ src/test/java/com/redhat/devtools/lsp4ij/mock/MockTextDocumentService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2022 Rogue Wave Software Inc. and others.
+ * Copyright (c) 2017, 2025 Rogue Wave Software Inc. and others.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
