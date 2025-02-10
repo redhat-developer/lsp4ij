989ae5abb58257cc6be76a09b19b8172b4b86d8b
azerr <azerr@redhat.com>
2025-02-10 15:42:40 +0100

    feat: manage unexpectedly stop of server
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/lsp4ij/settings/actions/ShowErrorLogAction.java
+++ src/main/java/com/redhat/devtools/lsp4ij/settings/actions/ShowErrorLogAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Red Hat, Inc.
+ * Copyright (c) 2025 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -10,42 +10,33 @@
  ******************************************************************************/
 package com.redhat.devtools.lsp4ij.settings.actions;
 
-import com.intellij.notification.Notification;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.redhat.devtools.lsp4ij.LanguageServerBundle;
+import com.redhat.devtools.lsp4ij.console.LSPConsoleToolWindowPanel;
 import com.redhat.devtools.lsp4ij.server.definition.LanguageServerDefinition;
-import com.redhat.devtools.lsp4ij.settings.ErrorReportingKind;
-import com.redhat.devtools.lsp4ij.settings.UserDefinedLanguageServerSettings;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Disable error reporting action.
+ * Select "log" tab (for a given language server definition) action.
  */
-public class DisableLanguageServerErrorAction extends AnAction {
+public class ShowErrorLogAction extends AnAction {
 
-    private final @NotNull Notification notification;
     private final @NotNull LanguageServerDefinition languageServerDefinition;
     private final @NotNull Project project;
 
-    public DisableLanguageServerErrorAction(@NotNull Notification notification,
-                                            @NotNull LanguageServerDefinition languageServerDefinition,
-                                            @NotNull Project project) {
-        super(LanguageServerBundle.message("action.language.server.error.reporting.disable.text"));
-        this.notification = notification;
+    public ShowErrorLogAction(@NotNull LanguageServerDefinition languageServerDefinition,
+                              @NotNull Project project) {
+        super(LanguageServerBundle.message("action.language.server.error.show.logs.text"));
         this.languageServerDefinition = languageServerDefinition;
         this.project = project;
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        UserDefinedLanguageServerSettings manager = UserDefinedLanguageServerSettings.getInstance(project);
-        manager.updateSettings(languageServerDefinition.getId(),
-                new UserDefinedLanguageServerSettings.LanguageServerDefinitionSettings()
-                        .setErrorReportingKind(ErrorReportingKind.none));
-        notification.expire();
+        LSPConsoleToolWindowPanel.selectLogTab(languageServerDefinition, project);
     }
 
     @Override
