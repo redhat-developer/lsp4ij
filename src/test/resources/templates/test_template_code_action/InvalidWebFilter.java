5d750b825ae0bbd831eea5f52c5131f8efded40b
dessina-devasia <143582034+dessina-devasia@users.noreply.github.com>
2025-02-13 17:02:49 +0530

    Added test classes for quickfix

---
+++ src/test/resources/templates/test_template_code_action/InvalidWebFilter.java
@@ -0,0 +1,11 @@
+package io.openliberty.sample.jakarta.servlet;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter()
+public abstract class InvalidWebFilter implements Filter {
+
+}
+
+
