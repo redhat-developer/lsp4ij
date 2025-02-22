80e46d32fc29bc579bf5be78b06aa9aceb584269
Scott Wells <scott@illuminatedcloud.com>
2025-02-22 16:06:18 -0600

    Prospective fix for an issue observed by a user during testing.

--- src/main/java/com/redhat/devtools/lsp4ij/features/semanticTokens/viewProvider/LSPSemanticTokenPsiReference.java
+++ src/main/java/com/redhat/devtools/lsp4ij/features/semanticTokens/viewProvider/LSPSemanticTokenPsiReference.java
@@ -14,20 +14,14 @@ package com.redhat.devtools.lsp4ij.features.semanticTokens.viewProvider;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReferenceBase;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.util.ArrayUtil;
-import com.intellij.util.containers.ContainerUtil;
 import com.redhat.devtools.lsp4ij.LSPIJUtils;
 import com.redhat.devtools.lsp4ij.features.navigation.LSPGotoDeclarationHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * A PSI reference to a semantic token declaration that defers uses
@@ -55,27 +49,8 @@ class LSPSemanticTokenPsiReference extends PsiReferenceBase<LSPSemanticTokenPsiE
         if (elementType == LSPSemanticTokenElementType.REFERENCE) {
             Editor editor = LSPIJUtils.editorForElement(element);
             if (editor != null) {
-                // Cache the resolved reference so that it only has to happen once until/unless the source/target file(s) change
-                return CachedValuesManager.getCachedValue(element, new CachedValueProvider<>() {
-                    @Override
-                    @Nullable
-                    public Result<PsiElement> compute() {
-                        PsiElement[] targets = LSPGotoDeclarationHandler.getGotoDeclarationTargets(element, element.getTextOffset());
-
-                        // Add both source and target files as cache dependencies
-                        Set<PsiElement> dependencies = new HashSet<>();
-                        dependencies.add(element.getContainingFile());
-                        if (!ArrayUtil.isEmpty(targets)) {
-                            for (PsiElement target : targets) {
-                                PsiFile targetFile = target != null ? target.getContainingFile() : null;
-                                ContainerUtil.addIfNotNull(dependencies, targetFile);
-                            }
-                        }
-
-                        PsiElement target = ArrayUtil.getFirstElement(targets);
-                        return target != null ? Result.create(target, dependencies.toArray()) : null;
-                    }
-                });
+                PsiElement[] targets = LSPGotoDeclarationHandler.getGotoDeclarationTargets(element, element.getTextOffset());
+                return ArrayUtil.getFirstElement(targets);
             }
         }
 
