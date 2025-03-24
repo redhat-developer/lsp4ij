c001acfdad8ee3379421db40f683d6b5543dc1e4
azerr <azerr@redhat.com>
2025-03-24 15:42:38 +0100

    fix: remove isInexAll to avoid freeze.
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/lsp4ij/features/semanticTokens/viewProvider/LSPSemanticTokensFileViewProviderHelper.java
+++ src/main/java/com/redhat/devtools/lsp4ij/features/semanticTokens/viewProvider/LSPSemanticTokensFileViewProviderHelper.java
@@ -21,7 +21,6 @@ import com.intellij.util.ThreeState;
 import com.intellij.util.containers.ContainerUtil;
 import com.redhat.devtools.lsp4ij.LSPIJUtils;
 import com.redhat.devtools.lsp4ij.client.features.EditorBehaviorFeature;
-import com.redhat.devtools.lsp4ij.client.indexing.ProjectIndexingManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -56,7 +55,7 @@ public class LSPSemanticTokensFileViewProviderHelper implements LSPSemanticToken
 
     @Override
     public boolean isEnabled() {
-        return !ProjectIndexingManager.isIndexingAll() && getFile() != null;
+        return getFile() != null;
     }
 
     @Override
