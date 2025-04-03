56b9931b39c8891da38b2fbbad88ac2feb00dc75
Stephane Bouchet <sbouchet@redhat.com>
2025-04-03 11:25:30 +0200

    chore: fix broken images in plugin description for marketplace
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,12 +97,14 @@ intellijPlatform {
         description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
-
             with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+                subList(indexOf(start) + 1, indexOf(end))
+                    .joinToString("\n")
+                    .replace("](./", "](https://raw.githubusercontent.com/redhat-developer/lsp4ij/main/")
+                    .let(::markdownToHTML)
             }
         }
 
