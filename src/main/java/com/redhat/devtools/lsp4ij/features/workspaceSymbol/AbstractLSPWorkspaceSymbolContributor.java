d88d57b4ed67d762b72207c345841385983abf72
azerr <azerr@redhat.com>
2025-05-21 09:15:08 +0200

    fix: LSP4IJ plugin causes IDEA to freeze when workspace symbol is requested
    Fixes #1048
    
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/lsp4ij/features/workspaceSymbol/AbstractLSPWorkspaceSymbolContributor.java
+++ src/main/java/com/redhat/devtools/lsp4ij/features/workspaceSymbol/AbstractLSPWorkspaceSymbolContributor.java
@@ -13,7 +13,6 @@ package com.redhat.devtools.lsp4ij.features.workspaceSymbol;
 import com.intellij.ide.util.gotoByName.ChooseByNamePopup;
 import com.intellij.navigation.ChooseByNameContributorEx;
 import com.intellij.navigation.NavigationItem;
-import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.Processor;
@@ -26,12 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import static com.redhat.devtools.lsp4ij.internal.CompletableFutures.isDoneNormally;
-import static com.redhat.devtools.lsp4ij.internal.CompletableFutures.waitUntilDone;
 
 /**
  * Abstract base class for LSP goto symbol contributors
@@ -52,57 +46,40 @@ abstract class AbstractLSPWorkspaceSymbolContributor implements ChooseByNameCont
         if (queryString == null) {
             queryString = "";
         }
-        List<WorkspaceSymbolData> items = getWorkspaceSymbols(queryString, true, project);
-        if (items != null) {
-            items.stream()
-                    .filter(data -> data.getFile() != null && scope.accept(data.getFile()))
-                    .map(NavigationItem::getName)
-                    .forEach(processor::process);
-        }
+        getWorkspaceSymbols(queryString, true, project)
+                .thenAccept(items -> {
+                    if (items != null) {
+                        items.stream()
+                                .filter(data -> data.getFile() != null && scope.accept(data.getFile()))
+                                .map(NavigationItem::getName)
+                                .forEach(processor::process);
+                    }
+                });
     }
 
     @Override
     public void processElementsWithName(@NotNull String name,
                                         @NotNull Processor<? super NavigationItem> processor,
                                         @NotNull FindSymbolParameters parameters) {
-        List<WorkspaceSymbolData> items = getWorkspaceSymbols(name, false, parameters.getProject());
-        if (items != null) {
-            items
-                    .stream()
-                    .filter(ni -> parameters.getSearchScope().accept(ni.getFile()))
-                    .forEach(processor::process);
-        }
+        getWorkspaceSymbols(name, false, parameters.getProject())
+                .thenAccept(items -> {
+                    if (items != null) {
+                        items
+                                .stream()
+                                .filter(ni -> parameters.getSearchScope().accept(ni.getFile()))
+                                .forEach(processor::process);
+                    }
+                });
     }
 
-    private List<WorkspaceSymbolData> getWorkspaceSymbols(@NotNull String name, boolean cancel, Project project) {
+    private CompletableFuture<List<WorkspaceSymbolData>> getWorkspaceSymbols(@NotNull String name, boolean cancel, Project project) {
         // Consume LSP 'workspace/symbol' request
         LSPWorkspaceSymbolSupport workspaceSymbolSupport = LSPWorkspaceSupport.getSupport(project).getWorkspaceSymbolSupport();
         if (cancel) {
             workspaceSymbolSupport.cancel();
         }
         LSPWorkspaceSymbolParams params = createWorkspaceSymbolParams(name);
-        CompletableFuture<List<WorkspaceSymbolData>> workspaceSymbolFuture = workspaceSymbolSupport.getWorkspaceSymbol(params);
-        try {
-            waitUntilDone(workspaceSymbolFuture);
-        } catch (
-                ProcessCanceledException e) {//Since 2024.2 ProcessCanceledException extends CancellationException so we can't use multicatch to keep backward compatibility
-            //TODO delete block when minimum required version is 2024.2
-            workspaceSymbolSupport.cancel();
-            return null;
-        } catch (CancellationException e) {
-            // cancel the LSP requests workspace/symbol
-            workspaceSymbolSupport.cancel();
-            return null;
-        } catch (ExecutionException e) {
-            LOGGER.error("Error while consuming LSP 'workspace/symbol' request", e);
-            return null;
-        }
-
-        if (isDoneNormally(workspaceSymbolFuture)) {
-            // workspace/symbol has been collected correctly
-            return workspaceSymbolFuture.getNow(null);
-        }
-        return null;
+        return workspaceSymbolSupport.getWorkspaceSymbol(params);
     }
 
     /**
