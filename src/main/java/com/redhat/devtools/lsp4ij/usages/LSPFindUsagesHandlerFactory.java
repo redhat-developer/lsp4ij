5dac5994a7151f07816c142ccdb190b0ae1516ea
Sergej Salnikov <skill@google.com>
2026-05-19 12:23:44 +0200

    Let `LSPFindUsagesHandler` handle only its own elements

--- src/main/java/com/redhat/devtools/lsp4ij/usages/LSPFindUsagesHandlerFactory.java
+++ src/main/java/com/redhat/devtools/lsp4ij/usages/LSPFindUsagesHandlerFactory.java
@@ -12,8 +12,6 @@ package com.redhat.devtools.lsp4ij.usages;
 
 import com.intellij.find.findUsages.FindUsagesHandler;
 import com.intellij.find.findUsages.FindUsagesHandlerFactory;
-import com.intellij.openapi.progress.ProcessCanceledException;
-import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.psi.PsiElement;
 import com.redhat.devtools.lsp4ij.client.ExecuteLSPFeatureStatus;
 import com.redhat.devtools.lsp4ij.client.indexing.ProjectIndexingManager;
@@ -26,36 +24,15 @@ import org.jetbrains.annotations.Nullable;
 public class LSPFindUsagesHandlerFactory extends FindUsagesHandlerFactory {
     @Override
     public boolean canFindUsages(@NotNull PsiElement element) {
-        // Execute the dummy LSP find usage handler to collect references, implementations
-        // with LSPUsageSearcher if file is associated to a language server.
-        if (ProjectIndexingManager.canExecuteLSPFeature(element.getContainingFile()) == ExecuteLSPFeatureStatus.NOW) {
-            // To avoid overriding existing FindUsagesHandlerFactory for the given PsiElement
-            // (ex: JavaFindUsagesHandlerFactory which is also defined as "last".)
-            // we check if it exists a FindUsagesHandlerFactory for the element.
-            return !canExternalFindUsages(element);
-        }
-        return false;
+        return element instanceof LSPUsageTriggeredPsiElement &&
+            // Execute the dummy LSP find usage handler to collect references, implementations
+            // with LSPUsageSearcher if file is associated to a language server.
+            ProjectIndexingManager.canExecuteLSPFeature(element.getContainingFile()) == ExecuteLSPFeatureStatus.NOW;
+
     }
 
     @Override
     public @Nullable FindUsagesHandler createFindUsagesHandler(@NotNull PsiElement element, boolean forHighlightUsages) {
         return new LSPFindUsagesHandler(element);
-    }
-
-    private boolean canExternalFindUsages(@NotNull PsiElement element) {
-        for (FindUsagesHandlerFactory factory : FindUsagesHandlerFactory.EP_NAME.getExtensions(element.getProject())) {
-            try {
-                if (!factory.equals(this) &&  factory.canFindUsages(element)) {
-                    return true;
-                }
-            }
-            catch (IndexNotReadyException | ProcessCanceledException e) {
-                throw e;
-            }
-            catch (Exception e) {
-
-            }
-        }
-        return false;
     }
 }
